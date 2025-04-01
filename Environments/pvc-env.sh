0527dc71642c0a69b68e62ad41f691962df3e593
Juliana Kwan <julianakwan123@gmail.com>
2025-04-01 17:59:02 +0100

    Make substantial updates for new version of Swirles
    Before we were using a cobbled together version of the machine with
    no head node and Slurm on desktops. I have now adjusted the
    instructions for the new and final version of Swirles.

--- Environments/pvc-env.sh
+++ Environments/pvc-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-module load intel/compiler-intel-llvm/2024.2.0
-module load intel/mpi/2021.13
-module load intel/mkl/2024.2
+module load intel/compiler-intel-llvm/2025.0.4
+module load intel/mpi/2021.14
+module load intel/mkl/2025.0    
 
 export I_MPI_PMI_LIBRARY=/usr/lib/x86_64-linux-gnu/libpmi2.so
 export I_MPI_OFFLOAD=1
