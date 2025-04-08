28c469567626f233c145c26b2d4547ece96b7f4f
Juliana Kwan <julianakwan123@gmail.com>
2025-04-08 11:19:51 +0100

    Update pvc submission scripts for a generic job and idefix

--- Examples/Slurm/pvc-submit-idefix.sh
+++ Examples/Slurm/pvc-submit-idefix.sh
@@ -2,17 +2,14 @@
 
 #SBATCH --partition pvc       #name of partition either pvc, ampere, genoa, spr 
 #SBATCH --nodes=1             #how many nodes (up to 3)
-#SBATCH --ntasks=2            #e.g. up to 112 per PVC node
+#SBATCH --ntasks=2            #up to 112 per node
 #SBATCH --cpus-per-task=1     #set to >1 to use OpenMP threads
 #SBATCH --time=00:10:00       
 #SBATCH --gres=gpu:2          #necessary for GPU resources, can go up to 4
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export MY_WORKING_DIRECTORY="please fill this in..."
-export MY_EXEC="...also fill this in.."
-export MY_OPTIONS="...fill in as well..."
 
-source pvc-env.sh #This can be found from the Environment directory of this GitHub repo
-cd ${MY_WORKING_DIRECTORY}
+source pvc-env.sh   #This can be found in the Environment directory of this GitHub repo
+cd /cephfs/home/jk945/idefix/test/MHD/disk #The is your own working directory
 
-srun --mpi=pmi2 ${MY_EXEC} ${MY_OPTIONS}
+srun --mpi=pmi2 ./idefix
