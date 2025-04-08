8feee2ba93211714f171c504c43531f63063d9a1
Juliana Kwan <julianakwan123@gmail.com>
2025-04-08 11:03:03 +0100

    Update environment for new Intel compilers

--- Environments/pvc-env.sh
+++ Environments/pvc-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-module load intel/compiler-intel-llvm/2025.0.4
-module load intel/mpi/2021.14
-module load intel/mkl/2025.0    
+module load intel/compiler/2025.1.0
+module load intel/mpi/2021.15
+module load intel/mkl/2025.1
 
 export I_MPI_PMI_LIBRARY=/usr/lib/x86_64-linux-gnu/libpmi2.so
 export I_MPI_OFFLOAD=1
--- Examples/Slurm/pvc-submit.sh
+++ Examples/Slurm/pvc-submit.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --partition pvc       #name of partition either pvc, ampere, genoa, spr 
 #SBATCH --nodes=1             #how many nodes (up to 3)
-#SBATCH --ntasks=28           #up to 112 per node
+#SBATCH --ntasks=2           #up to 112 per node
 #SBATCH --cpus-per-task=1     #set to >1 to use OpenMP threads
 #SBATCH --time=00:10:00       
-#SBATCH --gres=gpu:1          #necessary for GPU resources, can go up to 4
+#SBATCH --gres=gpu:2          #necessary for GPU resources, can go up to 4
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-source /home/jk945/swirles.sh
-cd /home/jk945/Swirles
+source pvc-env.sh
+cd /cephfs/home/jk945/idefix/test/MHD/disk
 
-mpirun -np 28 ./my_exec
+srun --mpi=pmi2 ./idefix
