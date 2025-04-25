7d8430bb726bd6983a7f6468bc99d96b7b6a3aaf
Juliana Kwan <julianakwan123@gmail.com>
2025-04-25 18:27:13 +0100

    Fix MPI issue by switching to torchrun Fix MPI issue by switching to torchrun
    Many thanks to Maxwell for the fix! srun was using the
    wrong version of MPI so the port address was always
    busy. The program now launches using torchrun which
    works out the version of MPI needed.

--- Examples/Slurm/ampere-submit-ml.sh
+++ Examples/Slurm/ampere-submit-ml.sh
@@ -2,16 +2,14 @@
 
 #SBATCH --partition ampere    #name of partition
 #SBATCH --nodes=1             #how many nodes (up to 3)
-#SBATCH --ntasks=1            #up to 48
-#SBATCH --cpus-per-task=1     #set to >1 to use OpenMP threads
+#SBATCH --ntasks-per-node=2   #same as number of GPUs
 #SBATCH --time=00:30:00       
-#SBATCH --gres=gpu:1          #up to 4
+#SBATCH --gres=gpu:2          #up to 4
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=1
 ### change 5-digit MASTER_PORT as you wish, slurm will raise Error if duplicated with others
-### change WORLD_SIZE as gpus/node * num_nodes
+
 export MASTER_PORT=12340
-export WORLD_SIZE=1   #number of MPI ranks
 
 ### get the first node name as master address - customized for vgg slurm
 echo "NODELIST="${SLURM_NODELIST}
@@ -19,15 +17,16 @@ master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
 export RANK=$SLURM_PROCID
-export WORLD_SIZE=$SLURM_NTASKS
+### WORLD_SIZE should be gpus/node * num_nodes
+export WORLD_SIZE=$(echo "$SLURM_NTASKS_PER_NODE * $SLURM_NNODES" | bc -l)
 
 
 export MY_WORKING_DIRECTORY=/cephfs/home/jk945/swirles-training/Examples/ML
-export MY_EXEC="python ./resnet_ddp.py"
-export MY_EXEC_OPTIONS=""
+export MY_PYTHON_SCRIPT="./resnet_ddp.py"
+export MY_OPTIONS=""
 
 source /cephfs/store/gr-eps1/jk945/.torch-cuda/bin/activate
 cd ${MY_WORKING_DIRECTORY}
 
 
-srun ${MY_EXEC} ${MY_EXEC_OPTIONS}
+torchrun --nnode=${SLURM_NNODES} --nproc_per_node=${SLURM_NTASKS_PER_NODE} ${MY_PYTHON_SCRIPT} ${MY_OPTIONS}
