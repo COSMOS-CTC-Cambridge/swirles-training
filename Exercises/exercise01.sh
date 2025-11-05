0b50b049edbac727606d32f7c22958e0b648bdb1
Juliana Kwan <julianakwan123@gmail.com>
2025-11-05 14:44:06 +0000

    Remove debugging comments

--- Exercises/exercise01.sh
+++ Exercises/exercise01.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --partition pvc       #name of partition either pvc, ampere, genoa, spr 
-#SBATCH --nodes=1             #how many nodes (up to 3)
-#SBATCH --ntasks=2            #e.g. up to 112 per PVC node
-#SBATCH --cpus-per-task=1     #set to >1 to use OpenMP threads
-#SBATCH --time=00:10:00       
-#SBATCH --gres=gpu:2          #necessary for GPU resources, can go up to 4
+#SBATCH --partition pvc                      #name of partition either pvc, ampere, genoa, spr 
+#SBATCH --nodes=<number of nodes>            #how many nodes (up to 3)
+#SBATCH --ntasks=<number of tasks>           #e.g. up to 112 per PVC node
+#SBATCH --cpus-per-task=<number of cpus>     #set to >1 to use OpenMP threads
+#SBATCH --time=hh:mm:ss       
+#SBATCH --gres=gpu:<number of gpus>          #necessary for GPU resources, can go up to 4
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MY_WORKING_DIRECTORY="please fill this in..."
